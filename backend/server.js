aaa6626f3235f8f5983e9c8cf6c6771f3ddac17b
Kerkar831152 <vithalkerkar831152@gmail.com>
2026-09-16 11:33:46 +0530

    Prepare backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,2 @@
 const app = require('./app');
-app.get('/',(req,res)=>{
-    res.send("Acadflow backend is running");
-})
-app.listen(3000,()=>{
-    console.log("Server running on http://localhost:3000");
-})
+module.exports = app;
